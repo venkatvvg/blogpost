12fe74140b79a0238674fd1b69ac23e9d325bc27
venkatavivekghanta <113364335+venkatvvg@users.noreply.github.com>
2023-11-24 03:18:40 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,14 @@
+// components/Header.js
 import Link from 'next/link';
+import { useState } from 'react';
 
-export default function Header({ name }) {
+export default function Header({ name, onSearch }) {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = () => {
+    onSearch(searchTerm);
+  };
+
   return (
     <header className="pt-20 pb-12">
       <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
@@ -15,6 +23,21 @@ export default function Header({ name }) {
             Go to Home
           </a>
         </Link>
+      </div>
+      <div className="flex justify-center mt-4">
+        <input
+          type="text"
+          placeholder="Search..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="mr-2 px-2 py-1 border border-gray-300"
+        />
+        <button
+          onClick={handleSearch}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Search
+        </button>
       </div>
     </header>
   );
